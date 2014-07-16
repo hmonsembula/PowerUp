947fccd14c85dc0c6594c3395928de2507413b16
Hugo Ibuo <h_monsembula@yahoo.com>
2014-07-16 12:59:33 +0200

    Power Up V2.0 Update 1
    Load shedding module dropdowns using static data

--- gen/com/tecco/powerup/R.java
+++ gen/com/tecco/powerup/R.java
@@ -9,6 +9,7 @@ package com.tecco.powerup;
 
 public final class R {
     public static final class array {
+        public static final int dept_array=0x7f070002;
         /**  Nav Drawer List Item Icons 
  Keep them in order as the titles are in 
          */
@@ -26,6 +27,7 @@ public final class R {
         public static final int list_background_pressed=0x7f040002;
         public static final int list_divider=0x7f040003;
         public static final int list_item_title=0x7f040000;
+        public static final int snow=0x7f040006;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -81,70 +83,85 @@ public final class R {
         public static final int username_textbox_only=0x7f020028;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f0a0016;
-        public static final int action_settings=0x7f0a0017;
-        public static final int button1=0x7f0a000f;
-        public static final int counter=0x7f0a000d;
+        public static final int action_forgot_password=0x7f0a001d;
+        public static final int action_settings=0x7f0a001e;
+        public static final int add=0x7f0a0019;
+        public static final int button1=0x7f0a0011;
+        public static final int childItem=0x7f0a000c;
+        public static final int counter=0x7f0a000f;
+        public static final int department=0x7f0a0018;
         public static final int drawer_layout=0x7f0a0008;
-        public static final int editText1=0x7f0a0010;
-        public static final int editText2=0x7f0a0014;
+        public static final int editText1=0x7f0a0012;
+        public static final int editText2=0x7f0a0016;
         public static final int email=0x7f0a0003;
-        public static final int expandableListView1=0x7f0a0011;
-        public static final int expandableListView2=0x7f0a0012;
-        public static final int expandableListView3=0x7f0a0013;
+        public static final int expandableListView1=0x7f0a0013;
+        public static final int expandableListView2=0x7f0a0014;
+        public static final int expandableListView3=0x7f0a0015;
         public static final int frame_container=0x7f0a0009;
-        public static final int icon=0x7f0a000b;
+        public static final int heading=0x7f0a001c;
+        public static final int icon=0x7f0a000d;
         public static final int list_slidermenu=0x7f0a000a;
         public static final int login=0x7f0a0005;
         public static final int login_form=0x7f0a0002;
         public static final int login_status=0x7f0a0000;
         public static final int login_status_message=0x7f0a0001;
+        public static final int myList=0x7f0a001b;
         public static final int password=0x7f0a0004;
+        public static final int product=0x7f0a001a;
         public static final int register_button=0x7f0a0006;
+        public static final int sequence=0x7f0a000b;
         public static final int sign_in_button=0x7f0a0007;
-        public static final int textView1=0x7f0a000e;
-        public static final int title=0x7f0a000c;
-        public static final int txtLabel=0x7f0a0015;
+        public static final int textView1=0x7f0a0010;
+        public static final int title=0x7f0a000e;
+        public static final int txtLabel=0x7f0a0017;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int drawer_list_item=0x7f030002;
-        public static final int fragment_balance=0x7f030003;
-        public static final int fragment_fault_report=0x7f030004;
-        public static final int fragment_home=0x7f030005;
-        public static final int fragment_load_shedding=0x7f030006;
-        public static final int fragment_notifications=0x7f030007;
-        public static final int fragment_vendors=0x7f030008;
+        public static final int child_row=0x7f030002;
+        public static final int drawer_list_item=0x7f030003;
+        public static final int fragment_balance=0x7f030004;
+        public static final int fragment_fault_report=0x7f030005;
+        public static final int fragment_home=0x7f030006;
+        public static final int fragment_load_shedding=0x7f030007;
+        public static final int fragment_notifications=0x7f030008;
+        public static final int fragment_vendors=0x7f030009;
+        public static final int group_heading=0x7f03000a;
     }
     public static final class menu {
         public static final int login=0x7f090000;
         public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f06000c;
-        public static final int action_register=0x7f06000a;
+        public static final int action_forgot_password=0x7f060010;
+        public static final int action_register=0x7f06000e;
         public static final int action_settings=0x7f060001;
-        public static final int action_sign_in_register=0x7f060009;
-        public static final int action_sign_in_short=0x7f06000b;
+        public static final int action_sign_in_register=0x7f06000d;
+        public static final int action_sign_in_short=0x7f06000f;
+        /**  Spinner Start 
+         */
+        public static final int add=0x7f060006;
         public static final int app_name=0x7f060000;
+        public static final int department_store=0x7f060009;
         /**  Content Description 
          */
         public static final int desc_list_item_icon=0x7f060005;
         public static final int drawer_close=0x7f060004;
         public static final int drawer_open=0x7f060003;
-        public static final int error_field_required=0x7f060011;
-        public static final int error_incorrect_password=0x7f060010;
-        public static final int error_invalid_email=0x7f06000e;
-        public static final int error_invalid_password=0x7f06000f;
+        public static final int error_field_required=0x7f060015;
+        public static final int error_incorrect_password=0x7f060014;
+        public static final int error_invalid_email=0x7f060012;
+        public static final int error_invalid_password=0x7f060013;
         public static final int hello_world=0x7f060002;
-        public static final int login_progress_signing_in=0x7f06000d;
-        public static final int main_activity=0x7f060012;
+        public static final int login_progress_signing_in=0x7f060011;
+        public static final int main_activity=0x7f060016;
+        public static final int menu_settings=0x7f060007;
+        public static final int product_hint=0x7f060008;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f060007;
-        public static final int prompt_password=0x7f060008;
-        public static final int title_activity_login=0x7f060006;
+        public static final int prompt_email=0x7f06000b;
+        public static final int prompt_password=0x7f06000c;
+        public static final int title_activity_login=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -173,6 +190,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
-        public static final int LoginFormContainer=0x7f080002;
+        public static final int LoginFormContainer=0x7f080003;
+        public static final int MyActionBar=0x7f080002;
     }
 }
